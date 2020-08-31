a1eecf23bc8c950167416c0df1ce160947de82ae
xjh1230 <xjh_0125@sina.com>
2020-08-31 15:29:55 +0800

    jwt作业

--- nodejs/06/exam/index.js
+++ nodejs/06/exam/index.js
@@ -1,22 +1,24 @@
-const crypto = require('crypto')
-module.exports.createToken = token => {
-    const ary = token.split('.')
-    if (ary.length !== 3) {
-        return
-    }
+const crypto = require("crypto");
+module.exports.createToken = (token) => {
+  const ary = token.split(".");
+  if (ary.length !== 3) {
+    return;
+  }
 
-    return {
-        getExp: () => {
-            // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSJOEEOESOEIOEAJEHOEPOOIOPPOEJOEEOOIOSOJEHJXIJEHJASOJSJAIJAXJHOOEPOOIOEXOEIOSSJPPOESOSSOPSJEHJIEOPJOSEOSEOSSOEXJHOOSEOEXOEEOEOJPPOOIOEXOPPOOOJHPOOEJPAJEHJPEOOAOOIOEIOSSJXJJHIJPEJPHJPH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHOEPOOIOPPOEJOEEOOIOSOJHOOSSOPEOEP
-            // ##END##
-        },
+  return {
+    getExp: () => {
+      // ##BEGIN## 代码已加密
+      //暗号：贪心算法
+      return JSON.parse(new Buffer(ary[1], "base64").toString())["exp"];
+      // ##END##
+    },
 
-        verify: key => {
-            const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
-            return hmac === ary[2] + '='
-            
-        }
-    }
-}
+    verify: (key) => {
+      const hmac = crypto
+        .createHmac("SHA256", key)
+        .update(ary[0] + "." + ary[1])
+        .digest("base64");
+      return hmac === ary[2] + "=";
+    },
+  };
+};

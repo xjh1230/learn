bc43db0da8aa531cd8d9cc06589d1699d8d02265
xjh1230 <xjh_0125@sina.com>
2021-03-03 09:08:41 +0800

    vuex梳理

--- vue/demo/src/kstore/index.js
+++ vue/demo/src/kstore/index.js
@@ -4,25 +4,25 @@ import Vuex from "./kvuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    counter: 1,
-    msg: "天王盖地虎"
-  },
-  actions: {
-    add({ commit }) {
-      setTimeout(() => {
-        commit("add", 2);
-      }, 1000);
+    state: {
+        counter: 1,
+        msg: "天王盖地虎"
+    },
+    actions: {
+        add({ commit }) {
+            setTimeout(() => {
+                commit("add", 2);
+            }, 1000);
+        }
+    },
+    mutations: {
+        add(state, payload = 1) {
+            state.counter += payload;
+        }
+    },
+    getters: {
+        doubleCounter: state => {
+            return state.counter * 2;
+        }
     }
-  },
-  mutations: {
-    add(state, payload = 1) {
-      state.counter += payload;
-    }
-  },
-  getters: {
-    doubleCounter: state => {
-      return state.counter * 2;
-    }
-  }
 });

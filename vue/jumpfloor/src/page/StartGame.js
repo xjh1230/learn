9c8db4fe6e1c545f6a2da96b4cb850f1328b8319
xjh1230 <xjh_0125@sina.com>
2021-03-04 18:31:20 +0800

    vue3 图形渲染

--- vue/jumpfloor/src/page/StartGame.js
+++ vue/jumpfloor/src/page/StartGame.js
@@ -1,41 +1,51 @@
-import {h,defineComponent} from '@vue/runtime-core';
+import { h, defineComponent } from '@vue/runtime-core';
 import bgmImg from "../../assets/bgm.jpg";
 import startBtn from "../../assets/startBtn.png";
 
 export default defineComponent({
-    setup(props,ctx){
+    setup(props, ctx) {
 
-        const handleClick=()=>{
-            ctx.emit("changePage","GamePage")
+        const handleClick = () => {
+            ctx.emit("changePage", "GamePage")
         }
-        return{
+        return {
             handleClick
         }
     },
-    
+
     render(ctx) {
-        return h("Container",[
-            h("Sprite",{texture:bgmImg}),
-            h("Sprite",{
-                texture:startBtn,
-                x:250,
-                y:650,
-                interactive:true,
-                onClick:ctx.handleClick
+        return h("Container", [
+            h("Sprite", { texture: bgmImg }),
+            h("Sprite", {
+                texture: startBtn,
+                x: 250,
+                y: 650,
+                interactive: true,
+                onClick: ctx.handleClick
             }),
-            h("Rect",{
-                x:50,
-                y:250,
-                height:80,
-                width:80
+            h("Rect", {
+                x: 350,
+                y: 550,
+                height: 110,
+                width: 120,
+                rectcolor: {
+                    color: 0xDC143C, //颜色
+                    alpha: Math.random(),
+                },
             }),
-            h("Circle",{
-                x:60,
-                y:450,
-                radius:40,
-                color:"#ddd",
-                height:180,
-                width:180
+            h("Circle", {
+                x: 250,
+                y: 550,
+                scale: { //可以放大缩小图片
+                    x: 0.5,
+                    y: 0.5,
+                },
+                circlestyle: {
+                    width: 20, //线条宽度
+                    color: 0xDC143C, //颜色
+                    alpha: Math.random(),
+                    radius: 50, //半径
+                },
             })
 
         ])
--- vue/jumpfloor/src/runtime-canvas/index.js
+++ vue/jumpfloor/src/runtime-canvas/index.js
@@ -1,93 +1,97 @@
 import { Texture, Sprite, Text, Container, Graphics } from "pixi.js";
 import { createRenderer } from "@vue/runtime-core";
 const eleMap = {
-  Container: "Container",
-  Sprite: "Sprite",
-  Rect: "Rect",
-  Circle: "Circle",
+    Container: "Container",
+    Sprite: "Sprite",
+    Rect: "Rect",
+    Circle: "Circle",
 };
 
 const render = createRenderer({
-  createElement(type) {
-    let ele;
-    switch (type) {
-      case eleMap.Container:
-        ele = new Container();
-        break;
-      case eleMap.Sprite:
-        ele = new Sprite();
-        break;
-      case eleMap.Rect:
-        // 创建一个矩形
-        ele = new Graphics();
-        ele.lineStyle(4, 0xff3300, 1);
-        // ele.beginFill(0x66ccff);
-        ele.drawRect(0, 0, 10, 10);
-        ele.endFill();
-        // ele.x = 0;
-        // ele.y = 0;
-        // Opt-in to interactivity
-        ele.interactive = true;
-        // Shows hand cursor
-        //ele.buttonMode = true;
-        break;
-      case eleMap.Circle:
-        ele = new Graphics();
-        ele.lineStyle(4, 0xff3300, 1);
-        // ele.beginFill(0x66ccff);
-        ele.drawCircle(100, 310, 10);
-        ele.endFill();
-        // ele.x = 0;
-        // ele.y = 0;
-        ele.interactive = true;
-        break;
-    }
-    return ele;
-  },
-  patchProp(el, key, prevValue, nextValue) {
-    switch (key) {
-      case "pos":
-        el.moveTo(nextValue.x, nextValue.y);
-        break;
-      case "texture":
-        el.texture = Texture.from(nextValue);
-        break;
-      case "onClick":
-        el.on("pointertap", nextValue);
-        break;
-      default:
-        console.log(arguments);
-        el[key] = nextValue;
-        break;
-    }
-  },
-  createText(text) {
-    return new Text(text);
-  },
-  setElementText(node, text) {
-    const cText = new Text(text);
-    node.addChild(cText);
-  },
-  insert(el, parent) {
-    // append
-    parent.addChild(el);
-  },
-  // 新加接口实现
-  // 处理注释
-  createComment() {},
-  // 获取父节点
-  parentNode() {},
-  // 获取兄弟节点
-  nextSibling() {},
-  // 删除节点时调用
-  remove(el) {
-    const parent = el.parent;
-    if (parent) {
-      parent.removeChild(el);
-    }
-  },
+    createElement(type) {
+        let ele;
+        switch (type) {
+            case eleMap.Container:
+                ele = new Container();
+                break;
+            case eleMap.Sprite:
+                ele = new Sprite();
+                break;
+            case eleMap.Rect:
+                // 创建一个矩形
+                ele = new Graphics();
+                // ele.lineStyle(4, 0xff3300, 1);
+                ele.beginFill(0x66ccff); //0x66ccff
+                ele.drawRect(0, 0, 1, 1);
+                ele.endFill();
+                ele.interactive = true;
+                break;
+            case eleMap.Circle:
+                ele = new Sprite();
+                let cir = new Graphics();
+                ele.circle = cir;
+                break;
+        }
+        return ele;
+    },
+    patchProp(el, key, prevValue, nextValue) {
+        switch (key) {
+            case "pos":
+                el.moveTo(nextValue.x, nextValue.y);
+                break;
+            case "texture":
+                el.texture = Texture.from(nextValue);
+                break;
+            case "onClick":
+                el.on("pointertap", nextValue);
+                break;
+            case "rectcolor":
+                el.beginFill(nextValue.color, nextValue.alpha); //0x66ccff
+                el.drawRect(0, 0, 1, 1);
+                el.endFill();
+                break;
+            case "circlestyle":
+                if (el.circle) {
+                    el.removeChild(el.circle);
+                    el.circle.lineStyle(nextValue);
+                    el.circle.drawCircle(0, 0, nextValue.radius);
+                    el.addChild(el.circle);
+                }
+                break;
+            default:
+                // console.log(arguments);
+                el[key] = nextValue;
+                break;
+        }
+    },
+    createText(text) {
+        return new Text(text);
+    },
+    setElementText(node, text) {
+        const cText = new Text(text);
+        node.addChild(cText);
+    },
+    insert(el, parent) {
+        // append
+        console.log(el);
+        parent.addChild(el);
+    },
+    // 新加接口实现
+    // 处理注释
+    createComment() {},
+    // 获取父节点
+    parentNode() {},
+    // 获取兄弟节点
+    nextSibling() {},
+    // 删除节点时调用
+    remove(el) {
+        const parent = el.parent;
+        if (parent) {
+            parent.removeChild(el);
+        }
+    },
 });
 
 export function createApp(rootComponent) {
-  return render.createApp(rootComponent);
+    return render.createApp(rootComponent);
 }
